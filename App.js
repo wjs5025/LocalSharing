7dafe013024a4cff2a1a63356db43b44e15742a7
jeonInhyeok <wjs_5025@naver.com>
2021-03-16 23:05:48 +0900

    Commit Test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const YourApp = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
-        Fucking React-
+        Fucking React
       </Text>
     </View>
   );
