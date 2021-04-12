cb745ab38b408bd3fae3fc4829911304c7124408
munkua <munkua26@gmail.com>
2021-04-12 17:35:47 +0900

    [0412]add_code_FlatList
    리스트뷰 추가 및 필요없는 코드 삭제.
    이미지파일추가.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { ScrollView, FlatList, Platform, StyleSheet, View, Text, Button } from 'react-native';
+import { ScrollView, FlatList, Platform, StyleSheet, View, Text, Button, TouchableOpacity, TouchableWithoutFeedback, Image } from 'react-native';
+
 import Swiper from "react-native-web-swiper";
 // 컴포넌트 임포트
 import FirstSlide from "./components/FirstSlide";
@@ -7,30 +8,12 @@ import Main from "./components/Main";
 import FlatListDemo from "./components/FlatListDemo";
 import BottomNavigator from "./components/BottomNavigator";
 
-
-/* 재현쓰 스크롤뷰
-const arr = [];
-for (let i = 0; i < 100; i++) {
-  arr.push(i);
-}
-
 class App extends Component {
   render() {
     return (
-      <FlatList
-        keyExtractor={item => item.toString()}
-        data={arr}
-        renderItem={({item}) => <FlatListDemo num={item} />}
-      />
-    );
-  }
-}
-*/
-class App extends Component {
-  render() {
-    return (
-      <FirstSlide/>
-//     <BottomNavigator/>
+//      <FirstSlide/>
+        //<BottomNavigator/>
+        <FlatListDemo/>
     );
   }
 }
--- components/FlatListDemo.js
+++ components/FlatListDemo.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity, TouchableWithoutFeedback, Image} from 'react-native';
 
-const FlatListDemo = ({num}) => {
-return (
-		<View style={styles.container}>
-			<Text style={styles.text}>{num}</Text>
-		</View>
-	);
-};
+class FlatListDemo extends Component{
+    constructor(){  // 객체 상태 초기화
+        super();
 
-const styles = StyleSheet.create({
-	container: {
-		borderBottomWidth: 1,
-		height: 100,
-	},
-	text: {
-		textAlign: 'center',
-		textAlignVertical: 'center',
-		fontSize: 50,
-	},
+        this.state={
+            datas3: [
+                {name:"sam", message:"Hello world",img: require('./baseline_face_black_18x.png')},
+                {name:"robin", message:"Hello rn",img: require('./baseline_face_black_18x.png')},
+                {name:"kim", message:"Hello react",img: require('./baseline_face_black_18x.png')},
+                {name:"hong", message:"Hello hybrid",img: require('./baseline_face_black_18x.png')},
+                {name:"rosa", message:"Hello ios",img: require('./baseline_face_black_18x.png')},
+                {name:"lee", message:"Hello rom",img: require('./baseline_face_black_18x.png')},
+                {name:"jack", message:"Hello tom",img: require('./baseline_face_black_18x.png')},
+                {name:"moana", message:"Hello native",img: require('./baseline_face_black_18x.png')},
+
+            ],
+        };
+    }
+
+    render(){ // 렌더링 해서 화면에 보여줄 컨텐츠들
+        return(
+            <View style={style.root}>
+                <Text style={style.titleText}>FlatList Test</Text>
+                <FlatList // FlatList 의 기본속성, data는 this.state처럼 가변한 부분에서 가져온다.
+                    data={this.state.datas3}
+                    renderItem={this.renderItem}  // this.state가 renderItem의 매개변수로 들어간다.
+                    keyExtractor={ item=> item.name }>  // item 각각을 구분해주는 역할
+                </FlatList>
+            </View>
+        );
+    }//render method ..
+
+    //멤버 메소드 - FlatList의 renderItem용
+    renderItem=({item})=>{
+        return(
+            <TouchableOpacity style={style.itemView} onPress={()=>{alert(item.name);}}>
+                <Image source={item.img} style={style.itemImg}></Image>
+                <View style={{flexDirection:'column'}}>
+                    <Text style={style.itemName}>{item.name}</Text>
+                    <Text style={style.itemMsg}>{item.message}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
+const style= StyleSheet.create({
+    root:{flex:1, padding:16,},
+    titleText:{
+        fontSize:24,
+        fontWeight:'bold',
+        textAlign:'center',
+        paddingBottom:16,
+    },
+    itemView:{
+        flexDirection:'row',
+        borderWidth:1,
+        borderRadius:4,
+        padding:8,
+        marginBottom:12,
+    },
+    itemImg:{
+        width:120,
+        height:100,
+        resizeMode:'cover',
+        marginRight:8,
+    },
+    itemName:{
+        fontSize:24,
+        fontWeight:'bold',
+    },
+    itemMsg:{
+        fontSize:16,
+        fontStyle:'italic',
+    },
 });
 
-export default FlatListDemo;
+export default FlatListDemo
