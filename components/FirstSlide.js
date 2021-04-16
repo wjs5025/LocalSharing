9718e459b31817caf581ac332870c097245e8f47
wjs5025 <wjs_5025@naver.com>
2021-04-17 00:35:58 +0900

    최초슬라이드 수정
    최초슬라이드 수정(배경색, 멘트 등)

--- components/FirstSlide.js
+++ components/FirstSlide.js
@@ -36,25 +36,23 @@ class FirstSlide extends Component {
           <Swiper
             index={1}
             >
-            <View style={[styles.slideContainer,styles.slide1]}>
+            <View style={[styles.slideContainer,styles.slideBackground]}>
               <Text style = {[styles.slideTextTitle]}>처음오셨군요?</Text>
             </View>
-            <View style={[styles.slideContainer,styles.slide2]}>
-              <Text>지금부터 푸드쉐어링에 대해 간단히 설명해 드릴게요!</Text>
-              <Text>푸드쉐어링이란?</Text>
-              <Text style = {[styles.slideTextTitle]}>{"\n"}"먹을 수 있지만 먹지 않는 음식을 나누어서{"\n"} 음식물쓰레기를 줄이기위한 취지의 환경운동"{"\n"}</Text>
-              <Text>을 뜻합니다.</Text>
+            <View style={[styles.slideContainer,styles.slideBackground]}>
+            <Text style={[styles.slideTextSub]}>지금부터 푸드쉐어링에 대해 간단히 설명해 드릴게요!</Text>
+              <Text style={[styles.slideTextSub]}>푸드쉐어링이란?</Text>
+              <Text style = {[styles.slideTextTitle]}>{"\n"}푸드쉐어링이란, {"\n"}먹을 수 있지만 먹지 않는 음식이나 식재료를 주변과 나누는 환경운동"{"\n"}</Text>
+              <Text style={[styles.slideTextSub]}>을 뜻합니다.</Text>
             </View>
-            <View style={[styles.slideContainer,styles.slide3]}>
-              <Text style={[styles.slideTextSub]}>그렇다면</Text>
-              <Text> "로컬쉐어링"이란?</Text>
-              <Text>{"\n"}"이러한 푸드쉐어링을 내 주변이웃과{"\n"} 손쉽게 진행할 수 있도록 도와주는 앱"{"\n"}</Text>
-              <Text>이에요 !</Text>
+            <View style={[styles.slideContainer,styles.slideBackground]}>
+              <Text style = {[styles.slideTextTitle]}>{"\n"}"당신 손안의 푸드쉐어링, LOCAL SHARING"{"\n"}</Text>
+              <Text style={[styles.slideTextSub]}>이러한 푸드쉐어링을 지역내에서 손쉽게 진행할 수 있도록 도와드릴게요 !</Text>
             </View>
-            <View style={[styles.slideContainer,styles.slide4]}>
+            <View style={[styles.slideContainer,styles.slideBackground]}>
               <View style={[styles.slideText]}>
-                <Text style={[styles.slideTextTitle]}>지금 바로{"\n"} 시작해보세요 !</Text>
-                <Button title="Hello" onPress={() => { this.props.navigation.navigate("Second")}}></Button>
+                <Text style={[styles.slideTextTitle]}>지금 바로{"\n"}시작해보세요 !{"\n"}</Text>
+                <Button title="카카오로 로그인하기" onPress={() => { this.props.navigation.navigate("Second")}}></Button>
               </View>
             </View>
           </Swiper>
@@ -73,24 +71,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  slide1: {
-    backgroundColor: "rgba(20,20,200,0.3)"
-  },
-  slide2: {
-    backgroundColor: "rgba(20,200,20,0.3)"
-  },
-  slide3: {
-    backgroundColor: "rgba(200,20,20,0.3)"
-  },
-  slide4: {
-    backgroundColor: "#903372",
+  slideBackground: {
+    backgroundColor: "#CF2A27"
   },
   slideTextTitle:{
-    color : "blue",
-    fontSize : 30,
+    color : "white",
+    fontWeight : "bold",
+    fontSize : 35,
   },
   slideTextSub :{
-    color : "red",
+    color : "white",
   }
 });
 export default Screen;
