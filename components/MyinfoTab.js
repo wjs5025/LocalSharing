0070838b5b5ed1e88d9e780e03a6478b20044279
wjs5025 <wjs_5025@naver.com>
2021-05-23 02:17:25 +0900

    5.23 MyinfoTab.js 변경
    5.23 MyinfoTab.js 변경

--- components/MyinfoTab.js
+++ components/MyinfoTab.js
@@ -1,16 +1,72 @@
-import React, {Component, useState} from 'react';
-import { TouchableHighlight, StyleSheet, View, Text, Image, FlatList } from 'react-native';
+import React ,{useState}from 'react';
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+import { State } from 'react-native-gesture-handler';
+import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
 
-function MyinfoTab() {
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    borderBottomWidth : 1,
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  profileArea:{
+    flex:1,
+    alignItems : 'center',
+    justifyContent : 'center'
+  },
+  image: {
+      marginTop : 45,
+      width : 200,
+      height : 200,
+      borderWidth : 1,
+      borderColor : "black",
+      borderRadius : 30,
+  },
+  UserName : {
+    flex:1,
+    marginTop : 15,
+    fontFamily: 'NanumSquare_acEB',
+    color : "black",
+    fontSize : 30,
+  },
+  FlatList:{
+      flex:1,
+      marginLeft : 30,
+      marginRight : 30
+  }
+});
+
+const MyinfoTab = () => {
+    const [Name, onChangeName] = useState('저니녁');
     return (
-        <View>
-            <Text></Text>
-            <FlatList>
-                    <Text>List1</Text>
-                    <Text>List2</Text>
-                    <Text>List3</Text>
-            </FlatList>
+        <View style={styles.container}>
+            <View style = {styles.profileArea}>
+                <Image style={styles.image} source={require('../image/저니녁.jpg')}/>
+                <Text style={styles.UserName}>{Name} 님</Text>
+            </View>
+        <FlatList
+            style={styles.FlatList}
+            data={[
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
         </View>
-    )
-}
+    );
+    }
+
 export default MyinfoTab;
